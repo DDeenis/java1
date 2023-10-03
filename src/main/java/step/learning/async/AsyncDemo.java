9a87e2823c034b333b897dd7e631071e4e5c6014
DDeenis <ddeenis46@gmail.com>
2023-10-03 21:01:06 +0100

    threads

--- src/main/java/step/learning/async/AsyncDemo.java
+++ src/main/java/step/learning/async/AsyncDemo.java
@@ -1,10 +1,23 @@
 package step.learning.async;
 
+import java.util.Locale;
+
 public class AsyncDemo {
     public void run() {
         System.out.println("Async demo");
-        //multiThreadDemo();
-        dzSequence();
+
+        sum = 100.0;
+        final int months = 12;
+        activeThreadsCount = months;
+        Thread[] threads = new Thread[months];
+        for (int i = 0; i < months; i++) {
+            threads[i] = new Thread(new MonthRate(i + 1));
+            threads[i].start();
+        }
+
+        for(int i = 0; i < maxNumbers; i++) {
+            new Thread(new NumberPrinter(i + 1)).start();
+        }
     }
 
     private void multiThreadDemo() {
@@ -83,5 +96,61 @@ public class AsyncDemo {
         }
 
         System.out.println("final");
+    }
+
+    private int activeThreadsCount;
+    private double sum;
+    private final Object sumLocker = new Object();
+    private final Object atcLocker = new Object();
+    class MonthRate implements Runnable {
+        private final int month;
+
+        public MonthRate(int month) {
+            this.month = month;
+        }
+
+        @Override
+        public void run() {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ignore) {
+            }
+
+            double p = 0.1;
+            double localSum;
+            synchronized (sumLocker) {
+                localSum = sum *= 1.0 + p;
+            }
+
+            System.out.printf(Locale.US, "Month: %02d | Percent: %.2f | Sum: %.2f%n", month, p, localSum);
+
+            synchronized (atcLocker) {
+                --activeThreadsCount;
+                if(activeThreadsCount == 0) {
+                    System.out.printf(Locale.US, "----------%ntotal: %.2f%n", sum);
+                }
+            }
+        }
+    }
+
+    private final Object numbersLocker = new Object();
+    private final int maxNumbers = 9;
+    class NumberPrinter implements Runnable {
+        private final int number;
+
+        public NumberPrinter(int number) {
+            this.number = number;
+        }
+
+        @Override
+        public void run() {
+            System.out.println(number);
+
+            synchronized (numbersLocker) {
+                if(number == maxNumbers) {
+                    System.out.printf("Last thread finished with number: %d%n", number);
+                }
+            }
+        }
     }
 }
